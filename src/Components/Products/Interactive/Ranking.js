f4a0be573d4e1f9527d03e50fefc10f48b2c26d2
briannawimer <wimerbr@gmail.com>
2023-08-14 10:44:43 -0700

    fix rankings

--- src/Components/Products/Interactive/Ranking.js
+++ src/Components/Products/Interactive/Ranking.js
@@ -1,32 +1,24 @@
 import React from 'react';
-import Chips1 from '../../Data/Chips1';
 import Chips2 from '../../Data/Chips2';
 import Chips3 from '../../Data/Chips3';
 import Interactive from '../../Interactive/Interactive.js';
 import NutrientLegend from '../../Interactive/Legend/NutrientLegend.js';
 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
+
 
 function Ranking() {
     const components = [
-        <Interactive nutrition={Chips1} title="Chips" value={1} />,
-        <Interactive nutrition={Chips2} title="Chips" value={1} />,
-        <Interactive nutrition={Chips3} title="Chips" value={1} />
+        <Interactive nutrition={Chips2} title="Chips A" value={1} />,
+        <Interactive nutrition={Chips3} title="Chips B" value={1} />
     ];
 
-    const shuffledComponents = shuffleArray(components);
+   
 
     return (
       <>
         <div className='interactive-container'>
           <div className='interactive-wrapper'>
-            {shuffledComponents}
+            {components}
           </div>
           <div className="nutrient-legend-wrapper">
             <NutrientLegend />
